4a7233717e9b7d2861d888f7b19b2ab22828682e
John DeWyze <john.dewyze@gmail.com>
2016-06-16 13:29:03 -0500

    Fix nil line printing error

--- lib/putter/print_strategy.rb
+++ lib/putter/print_strategy.rb
@@ -4,8 +4,9 @@ module Putter
   module PrintStrategy
     Default = Proc.new do |label, line, method, args, result|
       prefix = "\tPutter Debugging: #{label} ".colorize(:cyan)
-      suffix = " -- Method: :#{method}, Args: #{args}, Result: #{result}".colorize(:green)
-      puts prefix + "." + line + suffix
+      line = !line.nil? ? ".#{line} " : " "
+      suffix = "-- Method: :#{method}, Args: #{args}, Result: #{result}".colorize(:green)
+      puts prefix + line + suffix
     end
   end
 end
--- spec/print_strategy_spec.rb
+++ spec/print_strategy_spec.rb
@@ -3,7 +3,13 @@ RSpec.describe Putter::PrintStrategy do
     it "prints the output" do
       expect do
         Putter::PrintStrategy::Default.call "my_label", "/spec/print_strategy_spec.rb:5", :method, ["Hello", :World, 1], "Hello World!"
-      end.to output(/Putter Debugging: my_label .*\.\/spec\/print_strategy_spec\.rb:5.* -- Method: :method, Args: \["Hello", :World, 1\], Result: Hello World!/).to_stdout
+      end.to output(/Putter Debugging: my_label .*\.\/spec\/print_strategy_spec\.rb:5 .*-- Method: :method, Args: \["Hello", :World, 1\], Result: Hello World!/).to_stdout
+    end
+
+    it "works with empty lines" do
+      expect do
+        Putter::PrintStrategy::Default.call "my_label", nil, :method, ["Hello", :World, 1], "Hello World!"
+      end.to output(/Putter Debugging: my_label .*-- Method: :method, Args: \["Hello", :World, 1\], Result: Hello World!/).to_stdout
     end
   end
 end
