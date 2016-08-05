512d4808ff8b6adbfdb10c1ecec35a46c369ea0c
John DeWyze <john.dewyze@gmail.com>
2016-08-05 16:09:57 -0500

    Allow methods to be set in the WatcherData object
    Currently the WatcherData object only allows the label to be set.
    
    This commit allows methods to be set as well and follows the same logic
    as configuration and will ensure the methods contains an array.

--- lib/putter/watcher_data.rb
+++ lib/putter/watcher_data.rb
@@ -4,6 +4,7 @@ module Putter
 
     def initialize(options, klass)
       _set_label(options[:label], klass)
+      _set_methods(options[:methods])
     end
 
     def _set_label(label, klass)
@@ -11,6 +12,16 @@ module Putter
         @label = label
       else
         @label = klass.name
+      end
+    end
+
+    def _set_methods(methods)
+      if methods.nil?
+        @methods = []
+      elsif !methods.is_a?(Array)
+        @methods = [methods]
+      else
+        @methods = methods
       end
     end
   end
--- spec/watcher_data_spec.rb
+++ spec/watcher_data_spec.rb
@@ -1,6 +1,6 @@
 describe Putter::WatcherData do
   it "sets a label if provided" do
-    data = Putter::WatcherData.new({ label: "my_label" }, "Class")
+    data = Putter::WatcherData.new({ label: "my_label" }, nil)
 
     expect(data.label).to eq("my_label")
   end
@@ -9,5 +9,23 @@ describe Putter::WatcherData do
     data = Putter::WatcherData.new({}, Object)
 
     expect(data.label).to eq("Object")
+  end
+
+  it "sets the methods if provided" do
+    data = Putter::WatcherData.new({ methods: [:test]}, Object)
+
+    expect(data.methods).to contain_exactly(:test)
+  end
+
+  it "sets a single method as an array" do
+    data = Putter::WatcherData.new({ methods: :test}, Object)
+
+    expect(data.methods).to contain_exactly(:test)
+  end
+
+  it "sets an empty array if no methods are provided" do
+    data = Putter::WatcherData.new({}, Object)
+
+    expect(data.methods).to eq([])
   end
 end
