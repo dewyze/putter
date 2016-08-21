5241df2f20b26bbee4cd9b56a2836f6063dda750
John DeWyze <john.dewyze@gmail.com>
2016-08-21 12:59:05 -0500

    Create follower data class
    Currently, all of the label and method data related to a Follower is
    contained inside that class. Since that class is primarily responsible
    for being the follower and handling the method missing functionality and
    method definition, the data has been moved out into a separate class.
    
    This allows better test set up and better matches the watcher class and
    it's data class. If there is enough similar those can be combined into a
    class when this refactor is done.

---
+++ lib/putter/follower_data.rb
@@ -0,0 +1,42 @@
+module Putter
+  class FollowerData
+    attr_accessor :label
+
+    def initialize(object, proxy, options)
+      @proxy = proxy
+      @proxied_methods = options[:methods] || []
+      _set_label(options[:label], object)
+    end
+
+    def add_method?(method)
+      return true if _is_whitelisted_method?(method)
+      return false if _is_ignored_method?(method)
+      return false if @proxy.instance_methods.include?(method)
+      return true if @proxied_methods.empty?
+      return true if @proxied_methods.include?(method)
+    end
+
+    def _set_label(label, object)
+      if !label.nil?
+        @label = label
+      elsif
+        object.class == Class
+        @label = object.name
+      else
+        @label = object.class.name + " instance"
+      end
+    end
+
+    def _is_whitelisted_method?(method)
+      ::Putter.configuration.methods_whitelist.map(&:to_sym).include?(method.to_sym)
+    end
+
+    def _is_ignored_method?(method)
+      ::Putter.configuration.ignore_methods_from.each do |klass|
+        return true if klass.methods.include?(method.to_sym)
+        return true if klass.instance_methods.include?(method.to_sym)
+      end
+      return false
+    end
+  end
+end
---
+++ spec/follower_data_spec.rb
@@ -0,0 +1,69 @@
+RSpec.describe Putter::FollowerData do
+  describe "#label" do
+    it "sets a label if provided" do
+      data = Putter::FollowerData.new(nil, nil, { label: "my_label" })
+
+      expect(data.label).to eq("my_label")
+    end
+
+    it "uses the class name for classes if no label is provided" do
+      data = Putter::FollowerData.new(Object, nil, {})
+
+      expect(data.label).to eq("Object")
+    end
+
+    it "uses the instance name for instances if no label is provided" do
+      data = Putter::FollowerData.new(Object.new, nil, {})
+
+      expect(data.label).to eq("Object instance")
+    end
+  end
+
+  describe "#add_method?" do
+    it "returns true if the method is whitelisted" do
+      proxy = ::Putter::MethodProxy.new
+      Putter.configuration.ignore_methods_from = [Object]
+      Putter.configuration.methods_whitelist = [:to_s]
+      data = Putter::FollowerData.new(Object.new, proxy, {})
+
+      expect(data.add_method?(:to_s)).to be true
+    end
+
+    it "returns false if the method is ignored class method" do
+      proxy = ::Putter::MethodProxy.new
+      Putter.configuration.ignore_methods_from = [Object]
+      data = Putter::FollowerData.new(Object.new, proxy, {})
+
+      expect(data.add_method?(:to_s)).to be false
+    end
+
+    it "returns false if it is already on the proxy" do
+      proxy = Module.new do
+        def new_method
+        end
+      end
+
+      data = Putter::FollowerData.new(Object.new, proxy, {})
+
+      expect(data.add_method?(:new_method)).to be false
+    end
+
+    it "returns true if no method were specified" do
+      proxy = ::Putter::MethodProxy.new
+      data = Putter::FollowerData.new(Object.new, proxy, {})
+
+      expect(data.add_method?(:new_method)).to be true
+    end
+
+    it "returns true if method is specified" do
+      proxy = ::Putter::MethodProxy.new
+      data = Putter::FollowerData.new(
+        Object.new,
+        proxy,
+        { methods: [:new_method] },
+      )
+
+      expect(data.add_method?(:new_method)).to be true
+    end
+  end
+end
