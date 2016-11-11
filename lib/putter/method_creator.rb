006d1e0333ec35d26e870a83e905d64ad228a745
John DeWyze <john.dewyze@gmail.com>
2016-11-11 09:57:54 -0500

    Print line if local directory is present
    Currently, if putter is used in a local script, it would not print the
    file location because it would try and match against the current
    directory and then return a substring which would be empty.
    
    This commit checks to see if the string contained even if is the local
    directory before doing the substring. It will also then add a leading
    "./" if the line does not being with / as it would in normal cases.

--- lib/putter/method_creator.rb
+++ lib/putter/method_creator.rb
@@ -3,8 +3,10 @@ module Putter
     def add_putter_method_to_proxy(proxy, data)
       proxy.send(:instance_exec, data) do |data|
         define_method(data.method) do |*proxy_args, &blk|
-          line = caller.find {|call| call.match(data.stack_trace_ignore_regex)}
-          data.line = line.split(::Dir.pwd)[1]
+          data.line = caller.find {|call| call.match(data.stack_trace_ignore_regex)}
+          if data.line.include?(::Dir.pwd)
+            data.line = data.line.split(::Dir.pwd)[1]
+          end
           data.args = proxy_args.to_s
           data.result = super *proxy_args, &blk
           ::Putter.configuration.print_strategy.call data
--- lib/putter/print_strategy.rb
+++ lib/putter/print_strategy.rb
@@ -4,7 +4,13 @@ module Putter
   module PrintStrategy
     Default = Proc.new do |data|
       prefix = "\tPutter Debugging: #{data.label} ".colorize(:cyan)
-      line = !data.line.nil? ? ".#{data.line} " : " "
+      if data.line.nil?
+        line = " "
+      elsif data.line[0] != "/"
+        line = "./#{data.line} "
+      else
+        line = ".#{data.line} "
+      end
       suffix = "-- Method: :#{data.method}, Args: #{data.args}, Result: #{data.result}".colorize(:green)
       puts prefix + line + suffix
     end
